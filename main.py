e69b082d624f851eda9f4459265ae938feceacfc
nekrabster <mark_sa@bk.ru>
2025-06-20 02:29:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import certifi
 import os
-os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()os.environ["QASYNC_QT_API"] = "pyqt6"
+os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
+os.environ["QASYNC_QT_API"] = "pyqt6"
 import sys
 import asyncio
 from qasync import QEventLoop, QApplication

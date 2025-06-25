244adf3bc887f9fb368db2a26b9e6b87e415a84d
nekrabster <mark_sa@bk.ru>
2025-06-26 00:43:25 +0800

    Update decorators.py

--- ui/decorators.py
+++ ui/decorators.py
@@ -4,14 +4,14 @@ from functools import wraps
 def heavy_obfuscation(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if random.random() < 0.05:
-            time.sleep(random.uniform(0.1, 0.3))
+        if random.random() < 0.01:
+            time.sleep(random.uniform(0.01, 0.05))
         return func(*args, **kwargs)
     return wrapper
 def critical_function(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        if random.random() < 0.05:
-            time.sleep(random.uniform(0.1, 0.2))
+        if random.random() < 0.005:
+            time.sleep(random.uniform(0.01, 0.03))
         return func(*args, **kwargs)
     return wrapper 

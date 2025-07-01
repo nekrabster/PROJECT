b0fd93058a0c59d47e01fd527fc415ad7824f93d
nekrabster <mark_sa@bk.ru>
2025-07-02 00:46:35 +0800

    Update side.py

--- ui/side.py
+++ ui/side.py
@@ -132,19 +132,21 @@ class DispatcherLogo(QWidget):
             self.version_label.setText("Установка...")
             QApplication.processEvents()
             updater_code = r"""@echo off
-title Обновление программы
+            title Обновление программы
 
-:waitloop
-tasklist | find /i "Soft-K.exe" >nul 2>&1
-if not errorlevel 1 (
-    timeout /t 1 >nul
-    goto waitloop
-)
+            :waitloop
+            tasklist | find /i "Soft-K.exe" >nul 2>&1
+            if not errorlevel 1 (
+                echo Ждем завершения Soft-K.exe...
+                taskkill /f /im "Soft-K.exe" >nul 2>&1
+                timeout /t 1 >nul
+                goto waitloop
+            )
 
-move /Y "Soft-K_temp.exe" "Soft-K.exe" >nul 2>&1
-start "" "Soft-K.exe"
-del "updater.bat"
-"""
+            move /Y "Soft-K_temp.exe" "Soft-K.exe" >nul 2>&1
+            start "" "Soft-K.exe"
+            del "%~f0"
+            """
             with open(UPDATER_FILE, "w", encoding="utf-8") as f:
                 f.write(updater_code)
             QProcess.startDetached(UPDATER_FILE)
